ddf23f22ccb2f6594e903f97eeccd7ef38e36d77
icarrr <faizarsangster@gmail.com>
2018-06-28 02:10:23 +0800

    Belajar PHP - tipe data

---
+++ dasar-PHP/index.php
@@ -0,0 +1,29 @@
+<?php
+//integer
+$a1 = 100;
+echo $a1;
+
+//double/float/real
+$a2 = 1.00;
+echo "<br>";
+echo $a2;
+echo "<br>";
+
+//boolean
+$b = 10;
+if ($b > 15) {
+  // code...
+  echo "benar";
+} else {
+  // code...
+  echo "salah";
+}
+
+//null
+$c = null;
+
+//string
+$d = "saya seorang Panglima Setan";
+echo "<br>";
+echo $d;
+?>
