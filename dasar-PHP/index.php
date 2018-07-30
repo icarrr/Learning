1dbc4d829b99681e600a84f943e20ced2c700d98
icarrr <faizarsangster@gmail.com>
2018-07-31 06:00:43 +0700

    operator PHP

--- dasar-PHP/index.php
+++ dasar-PHP/index.php
@@ -1,7 +1,12 @@
 <?php
-const anu = 'Belajar constant';
-echo anu ;
-echo "<br/>";
-define('Anu', 'Belajar canstant menggunakan aturan lain');
-echo Anu;
+$x = 5;
+$y = 3;
+// operator aritmatika
+echo $x + $y, "<br/>"; //penjumlahan
+echo $x - $y, "<br/>"; //pengurangan
+echo $x * $y, "<br/>"; //perkalian
+echo $x / $y, "<br/>"; //pembagian
+echo $x % $y, "<br/>"; //modulus atau mod
+echo $x + -$y, " minus <br/>"; //minus
+echo $x - +$y, " plus <br/>"; //plus
  ?>
