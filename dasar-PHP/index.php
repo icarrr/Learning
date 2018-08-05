247d4caecdb58320ad5f2e1803d1f1bcf78f0534
icarrr <faizarsangster@gmail.com>
2018-08-06 05:50:04 +0700

    perulangan PHP

--- dasar-PHP/index.php
+++ dasar-PHP/index.php
@@ -1,38 +1,28 @@
 <?php
-$x = 'windows';
-echo "Anda terindikasi sebagai? ";
-switch ($x) {
-  case 'windows ori':
-    echo "Kemungkinan anda pengguna bajakan";
-    break;
-  case 'windows 7 oem':
-    echo "Kemungkinan windows anda original tapi software bajakan";
-    break;
-  default:
-    echo "Anda pengguna open source";
-    break;
+for ($x=1; $x <= 10 ; $x++) {
+  echo "ini baris : $x";
+  echo "<br/>";
 }
-echo "<br/>";
 
-$y = 60;
-switch (true) {
-  case ($y < 60):
-    echo "Anda dinyatakan tidak lulus";
-    break;
-  case ($y == 60):
-    echo "Anda dinyatakan lulus";
-    break;
-  case ($y > 60 && $y < 70):
-    echo "Anda dinyatakan lulus tapi miris gak lulus";
-    break;
-  case ($y == 70 && $y < 90):
-    echo "Anda dinyatakan lulus dengan nilai baik";
-    break;
-  case ($y > 90):
-    echo "Anda sangat hebat";
-    break;
-  default:
-    echo "Anda sangat sangat hebat";
-    break;
+$a = 0;
+while ($a < 10) {
+  echo "ini ke-$a <br/>";
+  $a++; //kalau ini dihilangkan, perulangan tidak akan berhenti
+}
+
+$b = 0;
+do {
+  echo "ini $b <br/>";
+  $b++; //kalau ini dihilangkan, perulangan tidak akan berhenti
+} while ($b < 10);
+
+$theme = [
+  "theme anu aa",
+  "theme anu bb",
+  "theme bb",
+  "theme aa"
+];
+foreach ($theme as $key ) {
+  echo "$key <br/>";
 }
  ?>
