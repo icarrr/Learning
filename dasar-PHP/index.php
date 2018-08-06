b3c3382ea0da5aa28f931eaa1c41a2f171de0c98
icarrr <faizarsangster@gmail.com>
2018-08-06 17:21:59 +0700

    function PHP

--- dasar-PHP/index.php
+++ dasar-PHP/index.php
@@ -1,33 +1,12 @@
 <?php
-$x = array("Icar", "siBunglon", "Olivia");
-$x[2] = "ganteng";
-for ($i=0; $i < count($x); $i++) {
-  echo $x[$i]."<br/>";
+function anu(){
+  echo "sibunglon";
+  echo "ganteng";
 }
-$y = [
-  "nama" => "icar",
-  "umur" => "18",
-  "asal" => "Bulukumba"
-];
-echo "Namanya ialah ".$y["nama"].", berumur ".$y["umur"]." tahun dan berasal dari ".$y["asal"];
-$z = [
-  [
-    "nama" => "icar",
-    "umur" => "18",
-    "asal" => "Bulukumba"
-  ],
-  [
-    "nama" => "setan BSD",
-    "umur" => "17",
-    "asal" => "anu"
-  ]
-];
+anu();
 echo "<br/>";
-$age = 17;
-if ($age <= 17) {
-  echo "Namanya ialah ".$z[1]["nama"].", berumur".$z[1]["umur"]." tahun dan berasal dari ".$z[1]["asal"];
-}else {
-  echo "Tidak ada data yang cocok";
+function sesi_kenalan($name = "Faizar Septiawan", $nickname = "icar"){
+  echo "Hallo, nama saya ".$name, " dan biasa dipanggil ".$nickname ;
 }
-
+sesi_kenalan();
  ?>
