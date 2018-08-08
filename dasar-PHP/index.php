6c6e004abe4d9cf613a892ec75af834bc7b1b769
icarrr <faizarsangster@gmail.com>
2018-08-08 16:06:15 +0700

    class OOP PHP

--- dasar-PHP/index.php
+++ dasar-PHP/index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Form Registration</title>
-  </head>
-  <body>
-    <h1>Silahkan registrasi dulu _^_</h1>
-    <?php
-    include 'form.php';
-     ?>
-  </body>
-</html>
+<?php
+  class olivia{
+    // isi class  
+  }
+ ?>
